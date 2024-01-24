bf1ca43679e8aa25f561573d01098f67e250dabf
cubickkk <157541443+cubickkk@users.noreply.github.com>
2024-01-24 11:15:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Your JavaScript code goes here
+function redirectToAnotherSite() {
+    window.location.href = "https://www.youtube.com/"; 
+}
+// Your JavaScript code goes here
+function redirectToAnotherSite(url) {
+    window.location.href = url;}
